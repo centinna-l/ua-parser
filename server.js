35079198f5aed47d86e48769e829ae17d1bccb50
centinna-l <jerryjoy99@gmail.com>
2022-02-26 01:31:31 +0530

    added routes

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+app.get("/", function (req, res) {
+  try {
+    console.log(`/ua-parser`, new Date().toLocaleDateString());
+    // var ua = parser(req.headers["user-agent"]);
+    return res.json({ message: "Hello World" });
+  } catch (e) {
+    return res.json({ error: "Internal Server Error" });
+  }
+});
 app.get("/ua-parser", function (req, res) {
   try {
     console.log(`/ua-parser`, new Date().toLocaleDateString());
@@ -15,7 +24,7 @@ app.get("/ua-parser", function (req, res) {
   }
 });
 
-app.listen(3000, (_) => {
+app.listen(8000, (_) => {
   // This is a comment
   console.log("Server running at Port: 8000");
 });
